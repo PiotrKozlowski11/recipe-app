b7e0741e843b79319cbc1db8ef63c45ba4556fdf
PiotrKozlowski11 <85372826+PiotrKozlowski11@users.noreply.github.com>
2021-11-25 09:51:54 +0100

    Added validation to RecipeController, RecipeCommand, wrote tests.

--- src/main/java/org/kozlowski/recipeapp/controllers/RecipeController.java
+++ src/main/java/org/kozlowski/recipeapp/controllers/RecipeController.java
@@ -8,12 +8,16 @@ import org.kozlowski.recipeapp.services.RecipeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 public class RecipeController {
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
@@ -33,7 +37,7 @@ public class RecipeController {
         model.addAttribute("recipe", new RecipeCommand());
         System.out.println("New Recipe");
 
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @RequestMapping("/recipe/{id}/update")
@@ -41,14 +45,20 @@ public class RecipeController {
 
         model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(id)));
 
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @PostMapping("/recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult result) {
+
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(objectError -> log.debug(objectError.toString()));
+
+            return RECIPE_RECIPEFORM_URL;
+        }
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/" + savedCommand.getId()+"/show/";
+        return "redirect:/recipe/" + savedCommand.getId() + "/show/";
     }
 
     @RequestMapping("/recipe/{id}/delete")
